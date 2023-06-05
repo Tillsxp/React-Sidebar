de37816df7e677225039754a213409d8db99407a
Tillsxp <matilda.89@live.se>
2023-06-05 15:52:18 +0200

    Changed styling

--- sidebar/src/components/ui/SideBar.jsx
+++ sidebar/src/components/ui/SideBar.jsx
@@ -1,18 +1,28 @@
 import './Sidebar.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faList, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
+  const hamburger = document.getElementById('nav-toggle');
+
   return (
     <>
       <nav>
         <button id="nav-toggle">
-          <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faList} />
         </button>
         <ul>
-          <li>Home</li>
-          <li>Contact</li>
+          <li>
+            <button>
+              <FontAwesomeIcon icon={faHouse} />
+            </button>
+          </li>
+          <li>
+            <button>
+              <FontAwesomeIcon icon={faUser} />
+            </button>
+          </li>
         </ul>
       </nav>
     </>
